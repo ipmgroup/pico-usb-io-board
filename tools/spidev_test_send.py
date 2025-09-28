6bbbcf2d1de3ef58a22b14d9b2ab6dfe496e7631
Ka Ru <ka_ru2003@msn.com>
2025-09-28 20:45:36 +0200

    tools: rename DLN spidev wrapper to tools.dln_spidev to avoid name clash with system spidev

--- tools/spidev_test_send.py
+++ tools/spidev_test_send.py
@@ -165,9 +165,9 @@ def main():
 
     # chosen == 'dln'
     try:
-        from tools.spidev import SpiDev
+        from tools.dln_spidev import SpiDev
     except Exception as e:
-        print('Failed to import DLN wrapper (tools.spidev):', e)
+        print('Failed to import DLN wrapper (tools.dln_spidev):', e)
         print('If you want DLN backend, install pyusb: pip3 install pyusb')
         sys.exit(1)
 
